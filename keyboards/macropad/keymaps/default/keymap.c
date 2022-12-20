dc270b07bb28633387de37f642f8529d58846ee6
Anikodi <78634764+anikodi0907@users.noreply.github.com>
2022-12-20 22:58:37 +0530

    Update keymap.c

--- keyboards/macropad/keymaps/default/keymap.c
+++ keyboards/macropad/keymaps/default/keymap.c
@@ -230,9 +230,11 @@ static void render_anim(void) {
 void oled_task_user(void) {
     render_anim();  // renders pixelart
 
-    oled_set_cursor(0, 0);                            // sets cursor to (row, column) using charactar spacing (5 rows on 128x32 screen, anything more will overflow back to the top)
-    sprintf(wpm_str, "WPM:%03d", get_current_wpm());  // edit the string to change wwhat shows up, edit %03d to change how many digits show up
-    oled_write(wpm_str, false);                       // writes wpm on top left corner of string
+//     oled_set_cursor(0, 0);                            // sets cursor to (row, column) using charactar spacing (5 rows on 128x32 screen, anything more will overflow back to the top)
+//     sprintf(wpm_str, "WPM:%03d", get_current_wpm());  // edit the string to change wwhat shows up, edit %03d to change how many digits show up
+//     oled_write(wpm_str, false);                       // writes wpm on top left corner of string
+    oled_write_P(PSTR("WPM: "), false);
+    oled_write(get_u8_str(get_current_wpm(), ' '), false);
 
     led_t led_state = host_keyboard_led_state();  // caps lock stuff, prints CAPS on new line if caps led is on
     oled_set_cursor(0, 1);
