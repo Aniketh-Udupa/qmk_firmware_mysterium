9bef1306b2bdfca412d4f81be7d74936b6ae7e71
Anikodi <78634764+anikodi0907@users.noreply.github.com>
2022-12-31 12:54:12 +0530

    Update keymap.c

--- keyboards/macropad/keymaps/via/keymap.c
+++ keyboards/macropad/keymaps/via/keymap.c
@@ -1,10 +1,5 @@
 #include QMK_KEYBOARD_H
-enum layer_names {
-    _DEFAULT, 
-    _CODE,
-    _NUM, 
-    _FUNC
-};
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -127,26 +122,35 @@ void led_set_user(uint8_t usb_led) {
 
 }
 //OLED stuff here
-#ifdef OLED_DRIVER_ENABLE
+#ifdef OLED_ENABLE
 
 
-bool oled_task_user(void) {
-       oled_set_cursor(0,1);
-    
-    switch (get_highest_layer(layer_state)) {
-        case _DEFAULT :
-            oled_write_P('Main',  false);
+#define L_BASE 0
+#define L_LOWER 2
+#define L_RAISE 4
+#define L_ADJUST 8
+
+void oled_render_layer_state(void) {
+    oled_write_P(PSTR("Layer: "), false);
+    switch (layer_state) {
+        case L_BASE:
+            oled_write_ln_P(PSTR("Default"), false);
             break;
-        case _CODE :
-            oled_write_P('Code',  false);
+        case L_LOWER:
+            oled_write_ln_P(PSTR("Lower"), false);
             break;
-        case _NUM :
-            oled_write_P('Number',  false);
+        case L_RAISE:
+            oled_write_ln_P(PSTR("Raise"), false);
             break;
-        case _FUNC :
-            oled_write_P('Function',  false);
+        case L_ADJUST:
+        case L_ADJUST|L_LOWER:
+        case L_ADJUST|L_RAISE:
+        case L_ADJUST|L_LOWER|L_RAISE:
+            oled_write_ln_P(PSTR("Adjust"), false);
             break;
     }
+}
+
     
     return false;
 }
